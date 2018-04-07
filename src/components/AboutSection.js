99ef533dae5bd0691034f8477e3930229405e2d3
GreatBearShark <m.wood0904@gmail.com>
2018-04-07 17:00:04 -0500

    fixed header z-index

---
+++ src/components/AboutSection.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AboutSection extends Component {
+  render() {
+    return (
+      <div class="about-section">
+        <h3>About</h3>
+        <div className="about-body">
+          {/* It me */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AboutSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import ProjectContainer from '../components/ProjectContainer';
 import Hero from '../components/Hero';
+import AboutSection from '../components/AboutSection';
 
 const IndexPage = () => (
   <Grid>
     <Hero />
+    <AboutSection />
     <ProjectContainer />
 
     <Link to="/websocket-demo/">View Websocket Demo</Link>
