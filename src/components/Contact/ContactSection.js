28dc577d376b2d76c619c9d2c73aed8282f2279d
GreatBearShark <m.wood0904@gmail.com>
2018-04-30 23:31:59 -0500

    added hidden form field

--- src/components/Contact/ContactSection.js
+++ src/components/Contact/ContactSection.js
@@ -8,7 +8,8 @@ const ContactSection = () => (
       </span>Contact
     </h2>
     <div className="contact-body">
-      <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="contact-form" value="contact" />
         <div className="form-row contact-info-row">
           <input className="form-name" type="text" name="name" placeholder="Name" />
           <input className="form-email" type="email" name="email" placeholder="Email" />
