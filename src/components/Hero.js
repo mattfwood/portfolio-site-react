47a7feb2b275d8ddea5287e25a12afb4848347ef
GreatBearShark <m.wood0904@gmail.com>
2018-05-02 18:19:52 -0500

    fixed hero width

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col } from 'react-flexbox-grid';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const Hero = props => (
   <div className={`Hero ${props.animation ? 'active' : ''}`}>
-    <Row middle="xs">
+    <Row middle="xs" className="no-margins">
       <Col xs={12}>
         <div className="hero-headline">
-          I am a Web Developer specializing in React, Node and
-              Single-Page Applications
+          I am a Web Developer specializing in React, Node and Single-Page
+          Applications
         </div>
       </Col>
     </Row>
-    {/* <div className="hero-animation hero-top" />
-    <div className="hero-animation hero-right" />
-    <div className="hero-animation hero-bottom" />
-    <div className="hero-animation hero-left" /> */}
   </div>
 );
 
