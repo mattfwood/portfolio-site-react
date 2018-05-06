5323a38b85fbf70f29e1a02ab5f945ea81b34599
mwood <matt@cratebind.com>
2018-05-05 21:45:21 -0500

    updated skills cards

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -11,60 +11,77 @@ const AboutSection = () => (
     </h2>
     <div className="about-body">
       <p>
-        I am a self-taught, full-stack Developer from Dallas, Texas. I
-        specialize in NodeJS, React, and building APIs for applications.
+        I am a self-taught, full-stack Developer from Dallas, Texas. I specialize in NodeJS, React,
+        and building APIs for applications.
       </p>
       <p>
-        I've created several full-stack web applications and built front-end for
-        many more web apps. I’ve also developed NPM packages such as a Node CLI
-        tools and a React boilerplate generator.
+        I've created several full-stack web applications and built front-end for many more web apps.
+        I’ve also developed NPM packages such as a Node CLI tools and a React boilerplate generator.
       </p>
       <p>
-        When I’m not spinning up a new project, I spend much of my free time
-        learning about the latest technology in web development and applying it
-        to existing projects. When I think I understand a new idea thoroughly,
-        I’ll usually make a blog post about it, too.
+        When I’m not spinning up a new project, I spend much of my free time learning about the
+        latest technology in web development and applying it to existing projects. When I think I
+        understand a new idea thoroughly, I’ll usually make a blog post about it, too.
       </p>
       <p>
-        I believe in creating interactive web apps that are responsive and
-        intuitive, as well as writing clean, maintainable code that’s thoroughly
-        tested.
+        I believe in creating interactive web apps that are responsive and intuitive, as well as
+        writing clean, maintainable code that’s thoroughly tested.
       </p>
     </div>
     <h3>Skills</h3>
     <div className="about-skills">
-    <Row>
-      <Col xs={12} md={6} lg={4}>
-      <div className="skill-group">
-        <h4>Front-end</h4>
-        <div>CSS3, Javascript / ES6, React, Vue</div>
-      </div>
-      </Col>
-      <Col xs={12} md={6} lg={4}>
-      <div className="skill-group">
-        <h4>Back-end</h4>
-        <div>NodeJS, ExpressJS, PostgreSQL, MongoDB</div>
-      </div>
-      </Col>
-      <Col xs={12} md={6} lg={4}>
-      <div className="skill-group">
-        <h4>CMS</h4>
-        <div>Wordpress, Contentful</div>
-      </div>
-      </Col>
-      <Col xs={12} md={6} lg={4}>
-      <div className="skill-group">
-        <h4>Apps</h4>
-        <div>React Native, Electron</div>
-      </div>
-      </Col>
-      <Col xs={12} md={6} lg={4}>
-      <div className="skill-group">
-        <h4>Cloud</h4>
-        <div>Firebase</div>
-      </div>
-      </Col>
-    </Row>
+      <Row>
+        <Col xs={12} md={6} lg={4}>
+          <div className="skill-group project-card">
+            <div className="card-header">
+              <h4>Front-end</h4>
+            </div>
+            <div className="project-body">
+              <div>CSS3, Javascript / ES6, React, Vue</div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} md={6} lg={4}>
+          <div className="skill-group project-card">
+            <div className="card-header">
+              <h4>Back-end</h4>
+            </div>
+            <div className="project-body">
+              <div>NodeJS, ExpressJS, PostgreSQL, MongoDB</div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} md={6} lg={4}>
+          <div className="skill-group project-card">
+            <div className="card-header">
+              <h4>CMS</h4>
+            </div>
+            <div className="project-body">
+              <div>Wordpress, Contentful</div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} md={6} lg={4}>
+          <div className="skill-group project-card">
+            <div className="card-header">
+              <h4>Apps</h4>
+            </div>
+            <div className="project-body">
+              <div>React Native, Electron</div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} md={6} lg={4}>
+          <div className="skill-group project-card">
+            <div className="card-header">
+              <h4>Cloud</h4>
+            </div>
+            <div className="project-body">
+              <div>Firebase</div>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   </div>
 );
--- src/components/Contact/ContactSection.js
+++ src/components/Contact/ContactSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row, Col } from 'react-flexbox-grid';
 
 const ContactSection = () => (
   <div className="contact-section">
@@ -10,10 +11,14 @@ const ContactSection = () => (
     <div className="contact-body">
       <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="contact-form" value="contact" />
-        <div className="form-row contact-info-row">
-          <input className="form-name" type="text" name="name" placeholder="Name" />
-          <input className="form-email" type="email" name="email" placeholder="Email" />
-        </div>
+        <Row className="form-row contact-info-row">
+          <Col xs={12} md={6}>
+            <input className="form-name" type="text" name="name" placeholder="Name" />
+          </Col>
+          <Col xs={12} md={6}>
+            <input className="form-email" type="email" name="email" placeholder="Email" />
+          </Col>
+        </Row>
         <div className="form-row">
           <textarea name="message" placeholder="Message" />
         </div>
