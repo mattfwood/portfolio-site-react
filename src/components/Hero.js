9c38d92d8387d2ec5a42f10f6fd96c0757c7dae6
mwood <matt@cratebind.com>
2018-05-07 21:40:03 -0500

    fixed window width

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,48 +6,50 @@ import Typist from 'react-typist';
 
 const codeString = "import { About, Skills, BlogPosts } from 'matt-wood';";
 
-const code = () => {
-  // on mobile, break code into multiple lines
-  if (window.innerWidth < 768) {
-    return (
-      <Typist>
-        import {'{'}
-        <Typist.Delay ms={200} />
-        <br />
-        {'  '}About,
-        <Typist.Delay ms={200} />
-        <br />
-        {'  '}Skills,
-        <Typist.Delay ms={200} />
-        <br />
-        {'  '}BlogPosts,
-        <Typist.Delay ms={200} />
-        <br />
-        {'}'} from 'matt-wood'
-      </Typist>
-    );
-  }
+const Hero = (props) => {
+  const code = () => {
+    // on mobile, break code into multiple lines
+    if (window.innerWidth < 768) {
+      return (
+        <Typist>
+          import {'{'}
+          <Typist.Delay ms={200} />
+          <br />
+          {'  '}About,
+          <Typist.Delay ms={200} />
+          <br />
+          {'  '}Skills,
+          <Typist.Delay ms={200} />
+          <br />
+          {'  '}BlogPosts,
+          <Typist.Delay ms={200} />
+          <br />
+          {'}'} from 'matt-wood'
+        </Typist>
+      );
+    }
 
-  return <Typist>{codeString}</Typist>;
-};
+    return <Typist>{codeString}</Typist>;
+  };
 
-const Hero = props => (
-  <div className={`Hero ${props.animation ? 'active' : ''}`}>
-    <Row bottom="xs" className="no-margins">
-      {/* <Col xs={12}> */}
-      <div className="hero-headline">
-        <div className="code-section">{code()}</div>
-        <div>Full-Stack Web Developer</div>
-        <div className="hero-subhead">
-          Specializing in <span className="hero-inline-light-blue">React, </span>
-          <span className="hero-inline-green">NodeJS</span> and
-          <span className="hero-inline-yellow"> Python</span>
+  return (
+    <div className={`Hero ${props.animation ? 'active' : ''}`}>
+      <Row bottom="xs" className="no-margins">
+        {/* <Col xs={12}> */}
+        <div className="hero-headline">
+          <div className="code-section">{code()}</div>
+          <div>Full-Stack Web Developer</div>
+          <div className="hero-subhead">
+            Specializing in <span className="hero-inline-light-blue">React, </span>
+            <span className="hero-inline-green">NodeJS</span> and
+            <span className="hero-inline-yellow"> Python</span>
+          </div>
         </div>
-      </div>
-      {/* </Col> */}
-    </Row>
-  </div>
-);
+        {/* </Col> */}
+      </Row>
+    </div>
+  );
+};
 
 Hero.propTypes = {
   animation: PropTypes.bool.isRequired,
