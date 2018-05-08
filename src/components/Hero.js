93b9d18f10f5b0814a66621937d0cbc94e14507a
mwood <matt@cratebind.com>
2018-05-07 21:18:01 -0500

    added mobile typing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,47 @@
 // irBlack
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Typist from 'react-typist';
 
 const codeString = "import { About, Skills, BlogPosts } from 'matt-wood';";
 
+const code = () => {
+  // on mobile, break code into multiple lines
+  if (window.innerWidth < 768) {
+    return (
+      <Typist>
+        import {'{'}
+        <Typist.Delay ms={200} />
+        <br />
+        {'  '}About,
+        <Typist.Delay ms={200} />
+        <br />
+        {'  '}Skills,
+        <Typist.Delay ms={200} />
+        <br />
+        {'  '}BlogPosts,
+        <Typist.Delay ms={200} />
+        <br />
+        {'}'} from 'matt-wood'
+      </Typist>
+    );
+  }
+
+  return <Typist>{codeString}</Typist>;
+};
+
 const Hero = props => (
   <div className={`Hero ${props.animation ? 'active' : ''}`}>
     <Row bottom="xs" className="no-margins">
       {/* <Col xs={12}> */}
       <div className="hero-headline">
-        <div className="code-section">
-          <Typist>
-            {codeString}
-          </Typist>
-        </div>
+        <div className="code-section">{code()}</div>
         <div>Full-Stack Web Developer</div>
         <div className="hero-subhead">
           Specializing in <span className="hero-inline-light-blue">React, </span>
-          <span className="hero-inline-yellow">NodeJS</span> and
-          <span className="hero-inline-red"> Python</span>
+          <span className="hero-inline-green">NodeJS</span> and
+          <span className="hero-inline-yellow"> Python</span>
         </div>
       </div>
       {/* </Col> */}
