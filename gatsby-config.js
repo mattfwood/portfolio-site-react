bafe1ef8b9a7f53e78dfb90301257f439a8863d8
mwood <matt@cratebind.com>
2018-05-08 15:15:41 -0500

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,24 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: 'wetfygr1eqh0',
